7eb59b2d2800b74229b292e06d18bb2cb313a7ac
Nikhilraghuwanshi04 <Nikhilraghuwanshi04@gmail.com>
2024-12-07 23:19:17 +0530

    Describe your changes briefly

--- src/components/LoginAuth/PageTitle.jsx
+++ src/components/LoginAuth/PageTitle.jsx
@@ -1,8 +1,8 @@
 export default function PageTitle() {
     return (
       <div className="absolute top-8 left-8">
-        <h1 className="text-2xl font-bold text-white bg-blue-600 px-6 py-3 rounded-full shadow-lg">
-          Test Cases for Login Page
+        <h1 className="text-2xl font-bold text-white  px-6 py-3 rounded-full shadow-lg">
+     
         </h1>
       </div>
     );
